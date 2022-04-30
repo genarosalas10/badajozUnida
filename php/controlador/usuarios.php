73075ba0ee64b8a8d7186ff2e7c3d665d4d4d875
genarosalas10 <genarosalas10@gmail.com>
2022-04-30 23:53:16 +0200

    añadido el index.php que habeis hecho

--- php/controlador/usuarios.php
+++ php/controlador/usuarios.php
@@ -4,6 +4,7 @@ require_once "c_Usuario.php";
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: *");
+header('Content-Type: application/json');
 
 $c_Usario = new C_Usuario();
 $_respuestas = new Respuestas;
@@ -17,7 +18,7 @@ if($_SERVER['REQUEST_METHOD'] =="POST"){
     $datosArray = $c_Usario ->login($postBody);
 
     //devolvemos una respuesta
-    header('Content-Type: application/json');
+
     if(isset($datosArray["result"]['error_id'])){
         $responseCode = $datosArray["result"]['error_id'];
     }else{
@@ -26,7 +27,6 @@ if($_SERVER['REQUEST_METHOD'] =="POST"){
     }
     echo json_encode($datosArray);
 }else{
-    header('Content-Type: application/json');
     $datosArray = $_respuestas->error_405();
     echo json_encode($datosArray);
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,34 +8,67 @@ import {AppService} from "../../services/app.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    loginForm!: FormGroup;
-    constructor(private formBuilder:FormBuilder, private appService:AppService) { }
+  forma!: FormGroup;
+  constructor(private formBuilder:FormBuilder, private appService:AppService) { }
 
-    ngOnInit(): void {
-            this.loginForm = this.formBuilder.group({
-            email:['',[Validators.required, Validators.email]],
-            password:['',[Validators.required, Validators.minLength(8)]]
-        })
-    }
 
-  enviar(loginForm: FormGroup){
-    let datos=loginForm.value
-    datos.tipo='login';
-    console.log(JSON.stringify(datos));
-    this.appService.getQuery2('http://localhost/badajozUnida/php2/controlador/index.php', JSON.stringify(datos))
-        .subscribe(response=>{
-            console.table(response);
-            if(response['status']!='error'){
-                alert('Hola'+' ' + response[0]['nombre'])
-            }else{
-                alert(response['result']['error_id']+" " + response['result']['error_msg'])
-            }
-        })
+  ngOnInit(): void {
+    this.crearFormulario();
+  }
+  // Crear formulario
+  crearFormulario() {
+    this.forma = this.formBuilder.group({
+      email:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required, Validators.minLength(8)]]
+
+    })
   }
 
+  //Coger los datos y comprobar si son correctos
+  guardar(forma: FormGroup){
+    console.log(JSON.stringify(forma.value));
+
+    if (forma.invalid || forma.pending) {
+      Object.values(forma.controls).forEach(control => {
+        if (control instanceof FormGroup)
+          this.guardar(control);
+        control.markAsTouched();
+      })
+      return;
+    }
+    this.ComprobarUsuario(forma);
+    //this.forma.reset();
+
+  }
+
+  //Validador
   validar(campo1: string){
-    let campo: any = this.loginForm.get(campo1);
+    let campo: any = this.forma.get(campo1);
     return !(campo.invalid && campo.touched);
   }
 
+  //LLamar a la Api para saber si el usuario existe
+  ComprobarUsuario(loginForm: FormGroup)
+  {
+    let datos=loginForm.value
+    datos.tipo='login';
+    this.appService.getQuery2(`index.php`, JSON.stringify(datos.value))
+      .subscribe(data =>
+        {
+          console.log(data);
+          /*
+          sessionStorage.setItem('id', id);
+          sessionStorage.setItem('correo', correo);
+
+           */
+          window.location.reload();
+        }
+        , async (errorServicio) =>
+        {
+          console.log(errorServicio);
+          //this.toast=true;
+
+
+        });
+  }
 }
