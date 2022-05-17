dcb118ba8d88c6b9d4f7e03e8951bb06bab5b69b
Genaro Salas <g.salas@preving.com>
2022-05-17 20:48:10 +0200

    coger categorias del back

--- src/app/components/categoria/categoria.component.ts
+++ src/app/components/categoria/categoria.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {AppService} from "../../services/app.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-categoria',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService:AppService, private router:Router) {
+    this.listadoCategoria();
+  }
 
   ngOnInit(): void {
   }
 
+  listadoCategoria()
+  {
+    let datos = {
+      tipo: "listadoCategoria"
+  }
+    console.log(JSON.stringify(datos));
+
+    this.appService.postQuery(datos)
+      .subscribe(data => {
+
+          if (data['status'] != 'error') {
+            console.log(data)
+          } else {
+            console.log(data)
+          }
+         
+        }
+        , async (errorServicio) =>
+        {
+          console.log('he fallado')
+          console.log(errorServicio);
+          //this.toast=true;
+
+
+        });
+
+  }
 }
