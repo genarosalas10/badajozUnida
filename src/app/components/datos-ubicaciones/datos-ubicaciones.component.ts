36da8298d22f35ed9b4b2950e3c74c2497aa09c5
Genaro Salas <g.salas@preving.com>
2022-05-25 18:57:29 +0200

    Añadido comentarios.

--- src/app/components/datos-ubicaciones/datos-ubicaciones.component.ts
+++ src/app/components/datos-ubicaciones/datos-ubicaciones.component.ts
@@ -1,180 +1,207 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { AppService } from "../../services/app.service";
-import {ActivatedRoute, Router} from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppService } from '../../services/app.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-datos-ubicaciones',
   templateUrl: './datos-ubicaciones.component.html',
-  styleUrls: ['./datos-ubicaciones.component.css']
+  styleUrls: ['./datos-ubicaciones.component.css'],
 })
 export class DatosUbicacionesComponent implements OnInit {
-
   forma!: FormGroup;
-  modal=new ModalComponent;
-  idUbicacion:any;
-  ubicacion:any;
-  constructor(private formBuilder: FormBuilder, private appService: AppService, private router: Router, private activatedRoute:ActivatedRoute) {
-    this.activatedRoute.params.subscribe(parametros => {
-      this.idUbicacion=parametros['idUbicacion'];
-      console.log(parametros)
-      if(parametros['idUbicacion']>0){
-        console.log('modificar')
+  modal = new ModalComponent();
+  idUbicacion: any;
+  ubicacion: any;
+
+  /**
+   * @ignore
+   */
+  constructor(
+    private formBuilder: FormBuilder,
+    private appService: AppService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe((parametros) => {
+      this.idUbicacion = parametros['idUbicacion'];
+      console.log(parametros);
+      if (parametros['idUbicacion'] > 0) {
+        console.log('modificar');
         this.sacarUbicacion(parametros['idUbicacion']);
       }
     });
-
   }
 
+  /**
+   * @ignore
+   */
   ngOnInit(): void {
     this.crearFormulario();
   }
 
+  /**
+   * Inizializa los validadores del formulario.
+   */
   crearFormulario() {
     this.forma = this.formBuilder.group({
       nombre: ['', [Validators.required, Validators.minLength(2)]],
       descripcion: ['', [Validators.required, Validators.minLength(10)]],
-      idUbicacion: ['0', [Validators.minLength(1)]]
-
-    })
+      idUbicacion: ['0', [Validators.minLength(1)]],
+    });
   }
 
-  //comprobar formulario
+  /**
+   * Comprueba los campos del formulario.
+   * @param forma - Campos del formulario
+   * @returns - void
+   */
   guardar(forma: FormGroup) {
-
-    console.log('guardarFormulario')
+    console.log('guardarFormulario');
 
     if (forma.invalid || forma.pending) {
-      Object.values(forma.controls).forEach(control => {
-        if (control instanceof FormGroup)
-          this.guardar(control);
+      Object.values(forma.controls).forEach((control) => {
+        if (control instanceof FormGroup) this.guardar(control);
         control.markAsTouched();
-      })
+      });
       return;
     }
-    console.log(forma.value)
+    console.log(forma.value);
     if (forma.value.idUbicacion == 0) {
       this.anadirUbicacion(forma);
     } else {
       this.modificarUbicacion(forma);
     }
-
   }
 
+  /**
+   * Valida un campo del formulario.
+   * @param campo1 - Campo del formulario
+   * @returns - Campo válido y no enfocado
+   */
   validar(campo1: string) {
     let campo: any = this.forma.get(campo1);
     return !(campo.invalid && campo.touched);
   }
 
-
-  //llamar a la api para crear categoria
+  /**
+   * Añade una nueva ubicación.
+   * @param forma - Datos de la ubicación
+   */
   anadirUbicacion(forma: any) {
-
-    let datos = forma.value
+    let datos = forma.value;
     datos.tipo = 'crearUbicacion';
     //console.log(JSON.stringify(datos));
 
-    this.appService.postQuery(datos)
-      .subscribe(data => {
+    this.appService.postQuery(datos).subscribe(
+      (data) => {
         console.log(data);
         if (data['status'] != 'error') {
-          console.log('data')
+          console.log('data');
           this.router.navigate(['/ubicacion']);
           //this.borrarForm();
-
         } else {
-          this.modal.generateModal(`Algo salió mal`, `${data['result']['error_msg']}`, 'De acuerdo', 'error');
-          console.log(data)
+          this.modal.generateModal(
+            `Algo salió mal`,
+            `${data['result']['error_msg']}`,
+            'De acuerdo',
+            'error'
+          );
+          console.log(data);
           //this.borrarForm();
         }
-
+      },
+      async (errorServicio) => {
+        console.log('he fallado');
+        console.log(errorServicio);
+        //this.borrarForm();
       }
-        , async (errorServicio) => {
-          console.log('he fallado')
-          console.log(errorServicio);
-          //this.borrarForm();
-
-
-        });
+    );
   }
 
-  //llamar a la api para  modificar categoria
+  /**
+   * Modifica una ubicación.
+   * @param forma - Datos de la ubicación
+   */
   modificarUbicacion(forma: any) {
-
-    let datos = forma.value
+    let datos = forma.value;
     datos.tipo = 'modificarUbicacion';
     //console.log(JSON.stringify(datos));
 
-    this.appService.postQuery(datos)
-      .subscribe(data => {
-          console.log(data);
-          if (data['status'] != 'error') {
-            console.log('data')
-            this.router.navigate(['/ubicacion']);
-            //this.borrarForm();
-
-          } else {
-            this.modal.generateModal(`Algo salió mal`, `${data['result']['error_msg']}`, 'De acuerdo', 'error');
-            console.log(data)
-            //this.borrarForm();
-          }
-
-        }
-        , async (errorServicio) => {
-          console.log('he fallado')
-          console.log(errorServicio);
+    this.appService.postQuery(datos).subscribe(
+      (data) => {
+        console.log(data);
+        if (data['status'] != 'error') {
+          console.log('data');
+          this.router.navigate(['/ubicacion']);
           //this.borrarForm();
-
-
-        });
+        } else {
+          this.modal.generateModal(
+            `Algo salió mal`,
+            `${data['result']['error_msg']}`,
+            'De acuerdo',
+            'error'
+          );
+          console.log(data);
+          //this.borrarForm();
+        }
+      },
+      async (errorServicio) => {
+        console.log('he fallado');
+        console.log(errorServicio);
+        //this.borrarForm();
+      }
+    );
   }
 
-  volver(){
-    this.router.navigate(['ubicacion'])
+  /**
+   * @ignore
+   */
+  volver() {
+    this.router.navigate(['ubicacion']);
   }
 
-
-  sacarUbicacion(idUbicacion:any) {
+  /**
+   * Obtiene los datos de una ubicación.
+   * @param idSubcategoria - ID de la ubicación
+   */
+  sacarUbicacion(idUbicacion: any) {
     let datos = {
-    tipo : 'sacarUbicacionId',
-      idUbicacion : idUbicacion
-    }
+      tipo: 'sacarUbicacionId',
+      idUbicacion: idUbicacion,
+    };
     console.log(JSON.stringify(datos));
 
-    this.appService.postQuery(datos)
-      .subscribe(data => {
-          console.log(data)
-          if (data['status'] != 'error') {
-            this.ubicacion=data[0];
-            this.cargarDatosFormulario()
-            //console.log(this.categoria);
-          } else {
-            //this.modal.generateModal(`Algo salió mal`, `${data['result']['error_msg']}`, 'De acuerdo', 'error');
-            console.log(data)
-          }
-
+    this.appService.postQuery(datos).subscribe(
+      (data) => {
+        console.log(data);
+        if (data['status'] != 'error') {
+          this.ubicacion = data[0];
+          this.cargarDatosFormulario();
+          //console.log(this.categoria);
+        } else {
+          //this.modal.generateModal(`Algo salió mal`, `${data['result']['error_msg']}`, 'De acuerdo', 'error');
+          console.log(data);
         }
-        , async (errorServicio) => {
-          console.log('he fallado')
-          console.log(errorServicio);
-          //this.toast=true;
-
-
-        });
-
-}
-  cargarDatosFormulario()  {
-    //console.log(this.categoria.nombre)
-    this.forma.reset({
-      nombre:this.ubicacion.nombre,
-      descripcion: this.ubicacion.descripcion,
-      idUbicacion: this.idUbicacion
-    });
-
-
+      },
+      async (errorServicio) => {
+        console.log('he fallado');
+        console.log(errorServicio);
+        //this.toast=true;
+      }
+    );
   }
 
-
+  /**
+   * En caso de edición, añade los valores anteriores de los campos al formulario.
+   */
+  cargarDatosFormulario() {
+    //console.log(this.categoria.nombre)
+    this.forma.reset({
+      nombre: this.ubicacion.nombre,
+      descripcion: this.ubicacion.descripcion,
+      idUbicacion: this.idUbicacion,
+    });
+  }
 }
-
--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {AppService} from "../../services/app.service";
-import {ModalComponent} from "../modal/modal.component";
-import {Router} from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppService } from '../../services/app.service';
+import { ModalComponent } from '../modal/modal.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
   forma!: FormGroup;
-  modal=new ModalComponent;
+  modal = new ModalComponent();
   /**
    * @ignore
    */
-  constructor(private formBuilder:FormBuilder, private appService:AppService, private router:Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private appService: AppService,
+    private router: Router
+  ) {}
 
   /**
    * @ignore
@@ -29,75 +33,109 @@ export class RegistroComponent implements OnInit {
    */
   crearFormulario() {
     this.forma = this.formBuilder.group({
-      nombre:['',[Validators.required, Validators.pattern(/^([a-zA-ZÀ-ÿ\u00f1\u00d1]{4,30})(\s{0,1}[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,29}){0,1}$/)]],
-      apellidos:['',[Validators.required, Validators.pattern(/^(([a-zA-ZÀ-ÿ\u00f1\u00d1]){4,30})(\s{0,1}[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,29}){0,1}$/)]],
-      email:['',[Validators.required, Validators.email, Validators.pattern(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i) ]],
-      password:['',[Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,20}$/)]],
-      password2:['',[Validators.required ]],
-      fechaNacimiento:[null,[Validators.required ]]
-
-    })
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^([a-zA-ZÀ-ÿ\u00f1\u00d1]{4,30})(\s{0,1}[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,29}){0,1}$/
+          ),
+        ],
+      ],
+      apellidos: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(([a-zA-ZÀ-ÿ\u00f1\u00d1]){4,30})(\s{0,1}[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,29}){0,1}$/
+          ),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(
+            /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+          ),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,20}$/
+          ),
+        ],
+      ],
+      password2: ['', [Validators.required]],
+      fechaNacimiento: [null, [Validators.required]],
+    });
   }
   /**
    * Comprueba que todos los campos introducidos sean correctos.
-   * 
+   *
    * @param forma - Campos del formulario
    * @returns - void
    */
-  guardar(forma: FormGroup){
-
+  guardar(forma: FormGroup) {
     if (forma.invalid || forma.pending) {
-      Object.values(forma.controls).forEach(control => {
-        if (control instanceof FormGroup)
-          this.guardar(control);
+      Object.values(forma.controls).forEach((control) => {
+        if (control instanceof FormGroup) this.guardar(control);
         control.markAsTouched();
-      })
+      });
 
       return;
     }
     this.registro(forma);
     //this.forma.reset();
-
   }
 
-/**
- * Llama a la API para dar de alta a un usuario.
- * @param loginForm - Campos del formulario
- */
-  registro(loginForm: FormGroup)
-  {
-    let datos=loginForm.value
-    datos.tipo='registro';
+  /**
+   * Llama a la API para dar de alta a un usuario.
+   * @param loginForm - Campos del formulario
+   */
+  registro(loginForm: FormGroup) {
+    let datos = loginForm.value;
+    datos.tipo = 'registro';
     console.log(JSON.stringify(datos));
-    this.appService.postQuery(datos)
-      .subscribe(data => {
-          if (data['status'] != 'error') {
-            this.modal.generateModal('Éxito', `Cuenta creada con éxito.`, 'De acuerdo', 'success');
-            setTimeout(() => {
-                this.router.navigate(['login'])
-              },
-              2000);
-          } else {
-            this.modal.generateModal('Algo salió mal', `${data['result']['error_msg']}`, 'De acuerdo', 'error');
-          }
-
+    this.appService.postQuery(datos).subscribe(
+      (data) => {
+        if (data['status'] != 'error') {
+          this.modal.generateModal(
+            'Éxito',
+            `Cuenta creada con éxito.`,
+            'De acuerdo',
+            'success'
+          );
+          setTimeout(() => {
+            this.router.navigate(['login']);
+          }, 2000);
+        } else {
+          this.modal.generateModal(
+            'Algo salió mal',
+            `${data['result']['error_msg']}`,
+            'De acuerdo',
+            'error'
+          );
         }
-        , async (errorServicio) =>
-        {
-          console.log('he fallado')
-          console.log(errorServicio);
-          //this.toast=true;
-
-
-        });
+      },
+      async (errorServicio) => {
+        console.log('he fallado');
+        console.log(errorServicio);
+        //this.toast=true;
+      }
+    );
   }
 
-   /**
+  /**
    * Valida que un campo del formulario sea correcto.
    * @param campo1 - Valor del campo
    * @returns - Campo válido y no enfocado
    */
-  validar(campo1: string){
+  validar(campo1: string) {
     let campo: any = this.forma.get(campo1);
     return !(campo.invalid && campo.touched);
   }
@@ -108,20 +146,19 @@ export class RegistroComponent implements OnInit {
   get comprobarPasswords() {
     let pass1 = this.forma.get('password')?.value;
     let pass2 = this.forma.get('password2')?.value;
-    return (pass1 === pass2) ? true : false;
+    return pass1 === pass2 ? true : false;
   }
 
   /**
    * Comprueba que el usuario sea mayor de 16 años.
    */
-  get comprobarEdad()
-  {
-    if(this.forma.get('fechaNacimiento')!= null){
-    let fechaNacimiento = this.forma.get('fechaNacimiento')?.value;
-    let convertirFecha = new Date(fechaNacimiento).getTime();
-    let timeDiff = Math.abs(Date.now() - convertirFecha);
-    let edad = Math.floor((timeDiff / (1000 * 3600 * 24))/365);
-    return (edad >= 16) ? true : false;
+  get comprobarEdad() {
+    if (this.forma.get('fechaNacimiento') != null) {
+      let fechaNacimiento = this.forma.get('fechaNacimiento')?.value;
+      let convertirFecha = new Date(fechaNacimiento).getTime();
+      let timeDiff = Math.abs(Date.now() - convertirFecha);
+      let edad = Math.floor(timeDiff / (1000 * 3600 * 24) / 365);
+      return edad >= 16 ? true : false;
     }
     return true;
   }
@@ -130,30 +167,28 @@ export class RegistroComponent implements OnInit {
    * Permite visualizar tu contraseña
    */
   showHide() {
-
     const input = <HTMLInputElement>document.getElementById('iPassword');
-    const i = <HTMLInputElement>document.getElementById("ieye");
-    if (input.type === "password") {
-      input.type = "text";
-      i.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+    const i = <HTMLInputElement>document.getElementById('ieye');
+    if (input.type === 'password') {
+      input.type = 'text';
+      i.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
     } else {
-      input.type = "password";
-      i.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
+      input.type = 'password';
+      i.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
     }
   }
   /**
    * @ignore
    */
   showHide2() {
-
     const input = <HTMLInputElement>document.getElementById('iPasswordRepeat');
-    const i = <HTMLInputElement>document.getElementById("ieye2");
-    if (input.type === "password") {
-      input.type = "text";
-      i.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+    const i = <HTMLInputElement>document.getElementById('ieye2');
+    if (input.type === 'password') {
+      input.type = 'text';
+      i.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
     } else {
-      input.type = "password";
-      i.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
+      input.type = 'password';
+      i.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
     }
   }
 }
