ff51ba97395c8e5c2da12e46ee7f0e4d11f57d0a
Genaro Salas <g.salas@preving.com>
2022-05-31 16:27:50 +0200

    Comentado

--- php/modelo/Respuestas.php
+++ php/modelo/Respuestas.php
@@ -1,15 +1,29 @@
 <?php
 
+/**
+ * Clase que devuelve respuestas de error personalizados
+ */
 class Respuestas
 {
     public $response = ["status" => "ok", "result" => array()];
+
+  /**
+   * Para devolver un error 405
+   * @return array devuelve el número de error y un mensaje de error
+   */
     public function error_405(){
         $this->response['status'] = "error";
         $this->response['result'] = array(
             "error_id" => "405",
-            "error_msg" => "Metodo no permitido");
+            "error_msg" => "Método no permitido");
         return $this->response;
     }
+
+  /**
+   * Para devolver un error 200
+   * @param $valor || Puede recibir un mensaje especifico
+   * @return array devuelve el número de error y un mensaje de error
+   */
     public function error_200($valor = "Datos incorrectos"){
         $this->response['status'] = "error";
         $this->response['result'] = array(
@@ -17,6 +31,11 @@ class Respuestas
             "error_msg" => $valor);
         return $this->response;
     }
+
+  /**
+   * Para devolver un error 400
+   * @return array devuelve el número de error y un mensaje de error
+   */
     public function error_400(){
         $this->response['status'] = "error";
         $this->response['result'] = array(
@@ -25,6 +44,11 @@ class Respuestas
         return $this->response;
     }
 
+  /**
+   * Para devolver un error 500
+   * @param $valor ||  puede recibir un mensaje especifico
+   * @return array devuelve el número de error y un mensaje de error
+   */
     public function error_500($valor = "Error interno en el servidor"){
         $this->response['status'] = "error";
         $this->response['result'] = array(
