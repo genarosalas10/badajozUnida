216b837de63aa007da51a81ed0b88a67c670136d
Genaro Salas <g.salas@preving.com>
2022-05-31 16:38:49 +0200

    Comentado el modelo

--- php/modelo/modelo.php
+++ php/modelo/modelo.php
@@ -3,7 +3,10 @@
 require_once "../operaciones/Procesos_bd.php";
 require_once "Respuestas.php";
 
-
+/**
+ * La clase modelo
+ * Se encarga de llamar a porcesos_bd.php para que haga lo relacionado con la base de datos
+ */
 class modelo
 {
     private $conexion;
@@ -13,6 +16,12 @@ class modelo
         $this->conexion = new Procesos_bd;
         $this->_respuestas = new Respuestas;
     }
+
+  /**
+   * Devuelve los los valores sacados en la ultima consulta en utf-8
+   * @param $array
+   * @return array
+   */
     private function convertirUTF8($array){
         array_walk_recursive($array, function (&$item, $key){
             if(!mb_detect_encoding($item,'utf-8',true)){
@@ -21,7 +30,12 @@ class modelo
         });
         return $array;
     }
-    //Sacar informacion de la base de datos
+
+  /**
+   * Para sacar datos de la base de datos
+   * @param $query
+   * @return array
+   */
     public function obtenerDatos($query){
         $results = $this->conexion->consulta($query);
         $resultArray = array();
@@ -31,11 +45,23 @@ class modelo
         return $this->convertirUTF8($resultArray);
     }
     //Borrado y modificado
+
+  /**
+   * Realiza la consulta y devuelve las filas afectadas
+   * Se usará para borra y modificar
+   * @param $query
+   * @return int
+   */
     public function nonQuery($query){
         $results = $this->conexion->consulta($query);
         return $this->conexion->filasAfectadas();
     }
-    //INSERT
+  /**
+   * Realiza la consulta y devuelve las filas afectadas
+   * Se usará para insertar
+   * @param $query
+   * @return int
+   */
     public function nonQueryId($query){
         $resultado = $this->conexion->consulta($query);
         $filas = $this->conexion->filasAfectadas();
@@ -48,6 +74,13 @@ class modelo
             return $resultado;
         }
     }
+
+  /**
+   * Para realizar consultas preparadas
+   * @param $query
+   * @param $valor
+   * @return array|string Devuelve el resultado o un mensaje de error
+   */
     public function nonQueryConsultaPreparada($query, $valor){
       $results=$this->conexion->consultaPreparada($query,"s",$valor);
       if(!$results){
@@ -59,15 +92,28 @@ class modelo
       }
       return $this->convertirUTF8($resultArray);
     }
-    //encriptar
+
+  /**
+   * Para encriptar
+   * @param $string
+   * @return string
+   */
     protected function encriptar($string){
         return md5($string);
     }
 
+  /**
+   *  Devuelve el id del error de la última consulta
+   * @return mixed
+   */
     public function errorId(){
      return $this->conexion->errno();
     }
 
+  /**
+   * Devuelve el mensaje de error de la última consulta
+   * @return mixed
+   */
     public function errorms(){
       return $this->conexion->error();
     }
