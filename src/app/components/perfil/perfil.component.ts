2487c32c85f7e05ea602f60fb58c4387e2c2a89d
Genaro Salas <g.salas@preving.com>
2022-06-01 16:12:43 +0200

    Añadido llamadas a la api.

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AppService} from "../../services/app.service";
+import {UsuarioService} from "../../services/usuario.service";
+import {ModalComponent} from "../modal/modal.component";
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +10,152 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-
-  constructor() { }
+  usuario:any;
+  forma!: FormGroup;
+  modal = new ModalComponent();
+  constructor(private formBuilder: FormBuilder,
+              private appService: AppService,
+              private usuarioService: UsuarioService) {
+    this.crearFormulario();
+    this.sacarUsuario(usuarioService.idUsuario);
+  }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Inizializa los validadores del formulario.
+   */
+  crearFormulario() {
+    this.forma = this.formBuilder.group({
+      nombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^([a-zA-ZÀ-ÿ\u00f1\u00d1]{4,30})(\s{0,1}[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,29}){0,1}$/
+          ),
+        ],
+      ],
+      apellidos: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            /^(([a-zA-ZÀ-ÿ\u00f1\u00d1]){4,30})(\s{0,1}[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,29}){0,1}$/
+          ),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern(
+            /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+          ),
+        ],
+      ]
+    });
+  }
+
+  /**
+   * Comprueba que todos los campos introducidos sean correctos.
+   *
+   * @param forma - Campos del formulario
+   * @returns - void
+   */
+  guardar(forma: FormGroup) {
+    if (forma.invalid || forma.pending) {
+      Object.values(forma.controls).forEach((control) => {
+        if (control instanceof FormGroup) this.guardar(control);
+        control.markAsTouched();
+      });
+
+      return;
+    }
+    this.modificarUsuario(forma);
+  }
+  /**
+   * Añade la información del usuario al formulario
+   */
+  cargarDatosFormulario() {
+    this.forma.reset({
+      nombre: this.usuario.nombre,
+      direccion: this.usuario.direccion,
+      email: this.usuario.email,
+    });
+  }
+
+  /**
+   * Obtiene los datos de una .
+   * @param idUsuario - ID del usuario
+   */
+  sacarUsuario(idUsuario: any) {
+    let datos = {
+      tipo: 'perfilUsuario',
+      idUsuario: idUsuario,
+    };
+    console.log(JSON.stringify(datos));
+
+    this.appService.postQuery(datos).subscribe(
+      (data) => {
+        console.log(data);
+        if (data['status'] != 'error') {
+          this.usuario = data[0];
+          this.cargarDatosFormulario();
+        } else {
+          this.modal.generateModal(`Algo salió mal`, `${data['result']['error_msg']}`, 'De acuerdo', 'error');
+          console.log(data);
+        }
+      },
+      async (errorServicio) => {
+        console.log('he fallado');
+        console.log(errorServicio);
+      }
+    );
+  }
+
+  /**
+   * Llama a la API para dar de modificar a un usuario.
+   * @param forma - Campos del formulario
+   */
+  modificarUsuario(forma: FormGroup) {
+    let datos = forma.value;
+    datos.tipo = 'modificarPerfilUsuario';
+    console.log(JSON.stringify(datos));
+    this.appService.postQuery(datos).subscribe(
+      (data) => {
+        if (data['status'] != 'error') {
+          this.modal.generateModal(
+            'Éxito',
+            `${data}`,
+            'De acuerdo',
+            'success'
+          );
+        } else {
+          this.modal.generateModal(
+            'Algo salió mal',
+            `${data['result']['error_msg']}`,
+            'De acuerdo',
+            'error'
+          );
+        }
+      },
+      async (errorServicio) => {
+        console.log('he fallado');
+        console.log(errorServicio);
+      }
+    );
+  }
+
+  /**
+   * Valida que un campo del formulario sea correcto.
+   * @param campo1 - Valor del campo
+   * @returns - Campo válido y no enfocado
+   */
+  validar(campo1: string) {
+    let campo: any = this.forma.get(campo1);
+    return !(campo.invalid && campo.touched);
+  }
 }
